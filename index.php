3c5d7fb74a33326fc6edd687ee23b27dbc0971ba
Jackson0419 <55591420+Jackson0419@users.noreply.github.com>
2024-03-29 23:44:54 +0800

    2

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 $isMob = is_numeric(strpos(strtolower($_SERVER["HTTP_USER_AGENT"]), "mobile")); 
  
 if($isMob){ 
-    header("Location: ./index.html");
-}else{ 
     header("Location: ./mobile.html");
+}else{ 
+    header("Location: ./index.html");
     }
 ?>
